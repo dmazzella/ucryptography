f8ddf1a3b808512f6986b201f197a81c113d7ef1
dmazzella <damianomazzella@gmail.com>
2019-10-31 18:41:13 +0100

    Aggiunti tests in ucryptography

--- tests/ucryptography/x509.py
+++ tests/ucryptography/x509.py
@@ -7,7 +7,47 @@ from cryptography import x509, hashes, serialization
 try:
     from util import loads_sequence
 except ImportError:
-    from ucryptography.util import loads_sequence
+    try:
+        from ucryptography.util import loads_sequence
+    except ImportError:
+        from ubinascii import a2b_base64
+        from uio import BytesIO
+
+        def loadf_sequence(f):
+            f.seek(f.read().index(b'-----BEGIN') or 0)
+            l = f.readline()
+            if not l.startswith(b'-----BEGIN'):
+                # not a pem
+                f.seek(0)
+                data = f.read()
+                if data.startswith(b'\x30'):
+                    return data
+                return a2b_base64(data)
+
+            # pem
+            lines = []
+            while 1:
+                l = f.readline()
+                if l == b'' or l.startswith(b'-----END'):
+                    break
+                lines.append(l)
+            return a2b_base64(b''.join(lines).replace(b'\n', b''))
+
+
+        def load_sequence(filename):
+            f = open(filename, 'rb')
+            try:
+                return loadf_sequence(f)
+            finally:
+                f.close()
+
+
+        def loads_sequence(s):
+            f = BytesIO(bytes(s, 'utf-8'))
+            try:
+                return loadf_sequence(f)
+            finally:
+                f.close()
 
 CERT_DER = loads_sequence('''-----BEGIN CERTIFICATE-----
 MIICiDCCAi+gAwIBAgIUEkh9KHsIlsR5m73KoHd9dnoaE+EwCgYIKoZIzj0EAwIw
