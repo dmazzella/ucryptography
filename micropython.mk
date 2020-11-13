a10f9fb07f474d0b10a9d10aa942521397f0a8ec
Damiano Mazzella <damianomazzella@gmail.com>
2020-11-13 20:13:11 +0100

    Spostati i file dedicati all'accelerazione hardware per la parte crittografica nelle cartelle dedicate

--- micropython.mk
+++ micropython.mk
@@ -9,43 +9,45 @@ endif
 CFLAGS_USERMOD += -DMICROPY_PY_UCRYPTOGRAPHY=1
 CFLAGS_USERMOD += -I$(MBEDTLS_DIR)/include
 CFLAGS_USERMOD += -I$(MOD_UCRYPTOGRAPHY_DIR)
+ifeq ($(MCU_SERIES),wb)
+    CFLAGS_USERMOD += -I$(MOD_UCRYPTOGRAPHY_DIR)/hw_alts/$(MCU_SERIES)/
+endif
 ifeq ($(MICROPY_SSL_MBEDTLS),)
     ifneq ($(wildcard $(MBEDTLS_DIR)/crypto/*),)
         CFLAGS_USERMOD += -I$(MBEDTLS_DIR)/crypto/include
     endif
     CFLAGS_USERMOD += -DMBEDTLS_USER_CONFIG_FILE='"modcryptography_config.h"'
 else
-ifeq ($(MCU_SERIES),wb)
-CFLAGS_USERMOD += -DMBEDTLS_GCM_ALT
-CFLAGS_USERMOD += -DMBEDTLS_AES_ALT
-CFLAGS_USERMOD += -DMBEDTLS_RSA_ALT
-CFLAGS_USERMOD += -DMBEDTLS_ECP_ALT
-CFLAGS_USERMOD += -DMBEDTLS_ECDSA_VERIFY_ALT
-CFLAGS_USERMOD += -DMBEDTLS_ECDSA_SIGN_ALT
-endif
+    ifeq ($(MCU_SERIES),wb)
+        CFLAGS_USERMOD += -DMBEDTLS_GCM_ALT
+        CFLAGS_USERMOD += -DMBEDTLS_AES_ALT
+        CFLAGS_USERMOD += -DMBEDTLS_RSA_ALT
+        CFLAGS_USERMOD += -DMBEDTLS_ECP_ALT
+        CFLAGS_USERMOD += -DMBEDTLS_ECDSA_VERIFY_ALT
+        CFLAGS_USERMOD += -DMBEDTLS_ECDSA_SIGN_ALT
+    endif
 endif
 
 SRC_USERMOD += $(MOD_UCRYPTOGRAPHY_DIR)/modcryptography.c
 ifeq ($(MICROPY_SSL_MBEDTLS),)
-$(foreach src, $(wildcard $(MBEDTLS_DIR)/library/*.c), $(eval SRC_USERMOD += $(src)))
-ifneq ($(wildcard $(MBEDTLS_DIR)/crypto/*),)
-    $(foreach src, $(wildcard $(MBEDTLS_DIR)/crypto/library/*.c), $(eval SRC_USERMOD += $(src)))
-    ifneq ($(filter $(MBEDTLS_DIR)/crypto/library/error.c,$(SRC_USERMOD)),)
-        SRC_USERMOD := $(filter-out $(MBEDTLS_DIR)/crypto/library/error.c, $(SRC_USERMOD))
+    $(foreach src, $(wildcard $(MBEDTLS_DIR)/library/*.c), $(eval SRC_USERMOD += $(src)))
+    ifneq ($(wildcard $(MBEDTLS_DIR)/crypto/*),)
+        $(foreach src, $(wildcard $(MBEDTLS_DIR)/crypto/library/*.c), $(eval SRC_USERMOD += $(src)))
+        ifneq ($(filter $(MBEDTLS_DIR)/crypto/library/error.c,$(SRC_USERMOD)),)
+            SRC_USERMOD := $(filter-out $(MBEDTLS_DIR)/crypto/library/error.c, $(SRC_USERMOD))
+        endif
     endif
-endif
-ifeq ($(MCU_SERIES),wb)
-    SRC_USERMOD += $(MOD_UCRYPTOGRAPHY_DIR)/aes_alt.c
-    SRC_USERMOD += $(MOD_UCRYPTOGRAPHY_DIR)/gcm_alt.c
-    SRC_USERMOD += $(MOD_UCRYPTOGRAPHY_DIR)/ecp_curves_alt.c
-    SRC_USERMOD += $(MOD_UCRYPTOGRAPHY_DIR)/ecp_alt.c
-    SRC_USERMOD += $(MOD_UCRYPTOGRAPHY_DIR)/rsa_alt.c
-    SRC_USERMOD += $(MOD_UCRYPTOGRAPHY_DIR)/ecdsa_alt.c
-
-    HAL_SRC_C += $(addprefix $(HAL_DIR)/Src/stm32$(MCU_SERIES)xx_,\
-        hal_cryp.c \
-        hal_cryp_ex.c \
-        hal_pka.c \
-    )
-endif
+    ifeq ($(MCU_SERIES),wb)
+        SRC_USERMOD += $(MOD_UCRYPTOGRAPHY_DIR)/hw_alts/$(MCU_SERIES)/aes_alt.c
+        SRC_USERMOD += $(MOD_UCRYPTOGRAPHY_DIR)/hw_alts/$(MCU_SERIES)/gcm_alt.c
+        SRC_USERMOD += $(MOD_UCRYPTOGRAPHY_DIR)/hw_alts/$(MCU_SERIES)/ecp_curves_alt.c
+        SRC_USERMOD += $(MOD_UCRYPTOGRAPHY_DIR)/hw_alts/$(MCU_SERIES)/ecp_alt.c
+        SRC_USERMOD += $(MOD_UCRYPTOGRAPHY_DIR)/hw_alts/$(MCU_SERIES)/rsa_alt.c
+        SRC_USERMOD += $(MOD_UCRYPTOGRAPHY_DIR)/hw_alts/$(MCU_SERIES)/ecdsa_alt.c
+        HAL_SRC_C += $(addprefix $(HAL_DIR)/Src/stm32$(MCU_SERIES)xx_,\
+            hal_cryp.c \
+            hal_cryp_ex.c \
+            hal_pka.c \
+        )
+    endif
 endif
